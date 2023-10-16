4679b90accb18f4ac3b479288800494d22b44c20
Logon PF Fiap <logonpflocal@fiap.com.br>
2023-10-16 10:19:53 -0300

    PROJETO FESTA 2MIA

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+//modulos
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+//routes
+//middlewares
+
+//config
+const dbName = "partytime";
+const port = 3000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res)=>{
+    res.json({message : "Rota de teste, será trocada"});
+});
+
+app.listen(port, ()=>{
+    console.log(`O backend está rodando na porta ${port}`);
+})
+
